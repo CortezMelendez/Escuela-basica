60c2a5a89cc88d813e1e9adc22d0a9c0ca835fe0
20253RD043@utez.edu.mx <20253rd011@utez.edu.mx>
2026-03-05 15:46:12 -0600

    Estableci los atributos en private y cree el constructor global

--- Alumno.java
+++ Alumno.java
@@ -1,3 +1,26 @@
 public class Alumno {
-    
+    /*
+    Atributos: nombre, matricula, calificacion1, calificacion2, calificacion3.
+• Métodos: constructor, calcularPromedio(), mostrarAlumno().
+• Conceptos: encapsulamiento, métodos, atributos.
+*/
+
+private String nombre;
+private String matricula;
+private double calificacion1;
+private double calificacion2;
+private double calificacion3;
+
+public void Alumno(){
+}
+
+public void Alumno2(String nombre, String matricula, double calificacion1, double calificacion2, double calificacion3){
+this.nombre=nombre;
+this.matricula=matricula;
+this.calificacion1=calificacion1;
+this.calificacion2=calificacion2;
+this.calificacion3=calificacion3;
+}
+
+
 }
