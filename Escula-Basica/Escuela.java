638e6361ac24898d17605e77aff985ecddcc8ec3
DiegoReyPikzo <20253rd022@utez.edu.mx>
2026-03-05 16:24:29 -0600

    Agregué el supercito

--- Escula-Basica/Escuela.java
+++ Escula-Basica/Escuela.java
@@ -3,19 +3,13 @@ import java.nio.channels.SelectableChannel;
 import java.util.Set;
 
 public class Escuela extends Alumno {
-
-    public Escuela (){
-        super();
+    //Supercito para mostrar posteriormente
+    public Alumno (String nombre, String matricula, double calificacion1, double calificacion2, double calificacion3){
+        super(nombre, matricula, calificacion1, calificacion2, calificacion3);
     }
     //Constructorcito vacio :D
-    public Escuela(String nombre, String matricula double calificacion1, double calificacion2, double calificacion3){
-        setNombre(nombre);
-        setMatricula(matricula);
-        setCalificacion1(calificacion1);
-        setCalificacion2(calificacion2);
-        setCalificacion3(calificacion3);
 
-    }
+    @Override 
     public void mostrarAlumno(){
         System.out.println("--------------------------");
         System.out.println("Nombre: " + getNombre());
@@ -23,7 +17,9 @@ public class Escuela extends Alumno {
         System.out.println("Primera Calificación: " + getCalificacion1());
         System.out.println("Segunda Calificación:" + getCalificacion2());
         System.out.println("Tercera Calificación: " + getCalificacion3());
-        System.out.println("Promedio total :D : ", calcularPromedio );
+        System.out.println("Promedio total :D : ", calcularPromedio() );
+        System.out.println("------------------");
     }
+    
     
 }
